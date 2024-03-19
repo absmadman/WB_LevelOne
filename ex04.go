76c2764f39e8f3d4d30fac83b4381c548f9d6b73
Hearts Fiddlesticks <heartsfi@un-o2.kzn.21-school.ru>
2024-03-19 19:55:15 +0300

    done 04

--- ex04.go
+++ ex04.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 type Worker struct {
 	id   int
@@ -22,21 +26,60 @@ func (w *Worker) Work() {
 	}
 }
 
-func main() {
+func ByChannels() {
 	num := 20
 	jnum := 100
 	jobs := make(chan int, jnum)
 	res := make(chan int, jnum)
+	flag := make(chan int)
+	defer close(res)
+	defer close(jobs)
 	for i := 1; i < num; i++ {
 		worker := NewWorker(i, jobs, res)
 		go worker.Work()
 	}
-	for i := 0; i <= jnum; i += 1 {
-		jobs <- i
+	go func() {
+		for i := 0; ; i += 1 {
+			go func() {
+				jobs <- i
+				fmt.Println(<-res)
+				fl := <-flag
+				if fl == 1 {
+					return
+				}
+			}()
+		}
+	}()
+	time.Sleep(time.Second * 5)
+	flag <- 1
+}
+
+func ByCtx() {
+	cont, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	num := 20
+	jnum := 100
+	jobs := make(chan int, jnum)
+	res := make(chan int, jnum)
+	defer close(res)
+	defer close(jobs)
+	defer cancel()
+	for i := 1; i < num; i++ {
+		worker := NewWorker(i, jobs, res)
+		go worker.Work()
 	}
-	close(jobs)
-	for i := 0; i <= jnum; i++ {
-		fmt.Println(<-res)
+	for i := 0; ; i += 1 {
+		select {
+		case <-cont.Done():
+			fmt.Println("DONE")
+			return
+		default:
+			jobs <- i
+			fmt.Println(<-res)
+		}
 	}
-	close(res)
+}
+
+func main() {
+	//ByChannels()
+	ByCtx()
 }
